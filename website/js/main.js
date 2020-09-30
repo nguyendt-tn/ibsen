b44d2f6ebbf79992576a9bc4ab145dfb2e3f2170
nguyendt-tn <nguyendt@ptd.edu.vn>
2020-09-30 21:27:37 +0700

    up

---
+++ website/js/main.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: { delay: 2000, disableOnInteraction: false },
+  });
